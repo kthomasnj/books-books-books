3f6cf0b967543e567fc3d723137ad01d6627fe95
Keith S Thomas <kthomasnj@hotmail.com>
2022-11-26 17:40:49 -0500

    Added code for Apollo Server

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,62 @@
+const { gql } = require ("apollo-server-express");
+
+
+const typeDefs = gql`
+type Query {
+    me: User
+}
+
+input BookInput {
+    authors: [String]
+    description: String!
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+}
+
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Mutation {
+    login(
+        email: String!,
+        password: String!
+    ): Auth
+
+    addUser (
+        username: String!,
+        email: String!,
+        password: String!
+    ): Auth
+
+    saveBook (
+        bookData: BookInput!
+    ): User
+
+    RemoveBook (
+        bookId: ID!
+    ): User
+}
+`
+
+module.exports = typeDefs;
